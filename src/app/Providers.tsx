5ed6f462d7887074ffa824771123aff8f44656e1
kyoshinoda <kyoshisew@gmail.com>
2023-08-24 01:12:37 -0400

    added theme toggle (still edge cases

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,6 +1,14 @@
 "use client";
+import { useState, useEffect } from "react";
 import { ThemeProvider } from "next-themes";
 function Providers({ children }: any) {
+  const [mounted, setMounted] = useState<boolean>(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return <>{children}</>;
+  }
   return <ThemeProvider>{children}</ThemeProvider>;
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/Header";
 import Main from "@/pages/Main";
-
+import ThemeSwitcher from "./ThemeSwitcher";
 export default function Home() {
   return (
     <>
-      <Header />
-      <Main />
+      <Header></Header>
+      <Main/>
+      <ThemeSwitcher/>
     </>
   );
 }
