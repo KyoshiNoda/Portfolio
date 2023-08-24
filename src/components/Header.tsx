e950f82f084bf27f92aa403702da7881f4fb8a22
kyoshinoda <kyoshisew@gmail.com>
2023-08-24 00:20:18 -0400

    added navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,33 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 import { userInfo, headerItems } from "../constants/constant";
 import { NavItems } from "@/Types/Header";
-type Props = {};
-
-function Header({}: Props) {
+import { BiMenu } from "react-icons/bi";
+function Header() {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
   return (
-    <header>
-      <div>
-        <h2>{userInfo.name}</h2>
+    <header className="bg-white p-6 md:flex w-full z-10 justify-between fixed top-0 text-black">
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-bold">{userInfo.name}</h2>
+        <BiMenu
+          size={30}
+          className="md:hidden"
+          onClick={() => setIsMobile((prev) => !prev)}
+        />
       </div>
-      <div>
-        {
-          Object.keys(headerItems).map(item => (
-            <a>{headerItems[item as keyof NavItems].label}</a>
-          ))
-        }
+      <div
+        className={`mr-8 md:space-x-6 mt-3 md:mt-0 ${
+          isMobile ? "block" : "hidden"
+        }`}
+      >
+        {Object.keys(headerItems).map((item) => (
+          <a
+            key={headerItems[item as keyof NavItems].label}
+            className="block md:inline-block cursor-pointer"
+          >
+            {headerItems[item as keyof NavItems].label}
+          </a>
+        ))}
       </div>
     </header>
   );
