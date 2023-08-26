e8e8e21ae92f63cb66b39d5f633bc88f6dfa4f2f
KyoshiNoda <kyoshisew@gmail.com>
2023-08-26 02:11:31 -0400

    added project setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import NavBar from "@/components/NavBar";
 import Main from "@/pages/Main";
 import About from "@/components/About";
+import Projects from "@/components/Projects";
 export default function Home() {
   return (
     <>
       <NavBar></NavBar>
       <Main />
       <About />
+      <Projects />
     </>
   );
 }
--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -6,8 +6,8 @@ export const userInfo: UserObject = {
     "https://media.licdn.com/dms/image/C4E03AQF7xB3WcNl6zQ/profile-displayphoto-shrink_400_400/0/1614718854555?e=1698278400&v=beta&t=YMr-yK8n_y75DFIhkPKgyV2TE56d9Ar1Yg92yIbCfp0",
   heading:
     "I am a passionate Computer Science student with a strong foundation in programming and problem-solving. Alongside my studies, I have actively shared my knowledge as a tutor for various CS courses. I also gained practical experience through a rewarding software engineering internship, where I contributed to real-world projects and honed my development skills.",
-  
-    about: `<p>I'm a junior in my second semester at <b>Farmingdale State College</b>, set to graduate in Fall 2023.</p>
+
+  about: `<p>I'm a junior in my second semester at <b>Farmingdale State College</b>, set to graduate in Fall 2023.</p>
     <p>I'm a dedicated developer who enjoys bringing ideas to life through coding.</p>
     <p> Outside of coding, I'm a casual soccer player and fan, and I also love to kick back and play video games.</p>`,
   skills: [
@@ -29,4 +29,11 @@ export const headerItems: NavItems = {
   home: { label: "Home", page: "home" },
   about: { label: "About", page: "about" },
   projects: { label: "Projects", page: "projects" },
+};
+
+export const projects = {
+  scheduleFinder: { image: "" },
+  gymPal: { image: "" },
+  prosCons: { image: "" },
+  steamTime: { image: "" },
 };
