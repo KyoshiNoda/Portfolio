ed3dbdd363ba68aff64fd38cc2bb60e88fc3e6ef
kyoshinoda <kyoshisew@gmail.com>
2023-08-26 11:33:33 -0400

    installed flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,14 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-  darkMode: "class"
-}
-
+  plugins: [require("flowbite/plugin")],
+  darkMode: "class",
+};
