e20dd701a8fc80ed93cf2aaa0f6c54f68cf8cc18
kyoshinoda <kyoshisew@gmail.com>
2023-08-26 13:29:25 -0400

    added new timeline

--- src/components/Carrer.tsx
+++ src/components/Carrer.tsx
@@ -1,65 +1,81 @@
 "use client";
 import { Timeline } from "flowbite-react";
 import { headerItems } from "@/constants/constant";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { MdOutlineWork } from "react-icons/md";
 export default function Carrer() {
   return (
     <section
       id={headerItems.carrer.page}
       className="flex-col justify-center md:flex-row"
     >
-      <div className="m-4 md:ml-20 sm:ml-12 md:w-1/2">
-        <div className = "mt-28">
-          <h1 className="text-4xl">Carrer</h1>
-        </div>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2010 - 2011"
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          icon={<MdOutlineWork />}
+        >
+          <h3 className="vertical-timeline-element-title dark:text-black">Art Director</h3>
+          <h4 className="vertical-timeline-element-subtitle dark:text-black font-semibold">
+            San Francisco, CA
+          </h4>
+          <p className="dark:text-black">
+            Creative Direction, User Experience, Visual Design, SEO, Online
+            Marketing
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2010 - 2011"
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          icon={<MdOutlineWork />}
+        >
+          <h3 className="vertical-timeline-element-title dark:text-black">Art Director</h3>
+          <h4 className="vertical-timeline-element-subtitle dark:text-black font-semibold">
+            San Francisco, CA
+          </h4>
+          <p className="dark:text-black">
+            Creative Direction, User Experience, Visual Design, SEO, Online
+            Marketing
+          </p>
+        </VerticalTimelineElement>
 
-        <Timeline className="w-screen">
-          <Timeline.Item>
-            <Timeline.Point />
-            <Timeline.Content>
-              <Timeline.Time>February 2022</Timeline.Time>
-              <Timeline.Title>
-                Application UI code in Tailwind CSS
-              </Timeline.Title>
-              <Timeline.Body>
-                <p>
-                  Get access to over 20+ pages including a dashboard layout,
-                  charts, kanban board, calendar, and pre-order E-commerce &
-                  Marketing pages.
-                </p>
-              </Timeline.Body>
-            </Timeline.Content>
-          </Timeline.Item>
-          <Timeline.Item>
-            <Timeline.Point />
-            <Timeline.Content>
-              <Timeline.Time>March 2022</Timeline.Time>
-              <Timeline.Title>Marketing UI design in Figma</Timeline.Title>
-              <Timeline.Body>
-                <p>
-                  All of the pages and components are first designed in Figma
-                  and we keep a parity between the two versions even as we
-                  update the project.
-                </p>
-              </Timeline.Body>
-            </Timeline.Content>
-          </Timeline.Item>
-          <Timeline.Item>
-            <Timeline.Point />
-            <Timeline.Content>
-              <Timeline.Time>April 2022</Timeline.Time>
-              <Timeline.Title>
-                E-Commerce UI code in Tailwind CSS
-              </Timeline.Title>
-              <Timeline.Body>
-                <p>
-                  Get started with dozens of web components and interactive
-                  elements built on top of Tailwind CSS.
-                </p>
-              </Timeline.Body>
-            </Timeline.Content>
-          </Timeline.Item>
-        </Timeline>
-      </div>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2010 - 2011"
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          icon={<MdOutlineWork />}
+        >
+          <h3 className="vertical-timeline-element-title dark:text-black">Art Director</h3>
+          <h4 className="vertical-timeline-element-subtitle dark:text-black font-semibold">
+            San Francisco, CA
+          </h4>
+          <p className="dark:text-black">
+            Creative Direction, User Experience, Visual Design, SEO, Online
+            Marketing
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          date="2011 - present"
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          icon={<MdOutlineWork />}
+        >
+          <h3 className="vertical-timeline-element-title">Creative Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <p className="dark:text-black">
+            Creative Direction, User Experience, Visual Design, Project
+            Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
     </section>
   );
 }
