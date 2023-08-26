cd73f6a29510335cec4591dc5b944a87961c8268
KyoshiNoda <kyoshisew@gmail.com>
2023-08-26 14:48:54 -0400

    temp css fix

--- src/components/Carrer.tsx
+++ src/components/Carrer.tsx
@@ -10,9 +10,10 @@ export default function Carrer() {
   return (
     <section
       id={headerItems.carrer.page}
-      className="flex-col justify-center md:flex-row"
+      className="flex-col justify-center items-center md:flex-row"
     >
-      <VerticalTimeline>
+      <h1 className="text-6xl my-6 mt-3 invisible">Carrer</h1>
+      <VerticalTimeline className=" space-y-24 mt-28">
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="2021 - 2023"
@@ -58,28 +59,27 @@ export default function Carrer() {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-          date="2011 - present"
+          contentArrowStyle={{ borderRight: ")" }}
+          date="2023 - present"
           iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
           icon={<MdOutlineWork />}
         >
-          <h1 className="vertical-timeline-element-title dark:text-black text-2xl">
+          <h1 className="vertical-timeline-element-title text-2xl">
             Software Engineer Intern
           </h1>
-          <h2 className="text-sm">SAS Inc.</h2>
-          <h4 className="vertical-timeline-element-subtitle dark:text-black font-semibold">
+          <h1 className="text-xl">SAS Institute</h1>
+          <h4 className="vertical-timeline-element-subtitle font-semibold">
             Cary, NC
           </h4>
-          <p className="dark:text-black">
-            ●  Handled full stack development for license generations for all
-            SAS software. <br/>
-            ● Developed responsive user-friendly front-end
-            components with Angular with Typescript that handled license
-            metadata.<br/>
-            ● Implemented RESTful APIs using Spring Boot that dealt
-            with license expirations. <br/>
-            ● Developed comprehensive unit tests for
-            both front-end and back-end, ensuring a robust and error-free
+          <p>
+            ● Handled full stack development for license generations for all SAS
+            software. <br />
+            ● Developed responsive user-friendly front-end components with
+            Angular with Typescript that handled license metadata.
+            <br />
+            ● Implemented RESTful APIs using Spring Boot that dealt with license
+            expirations. <br />● Developed comprehensive unit tests for both
+            front-end and back-end, ensuring a robust and error-free
             application.
           </p>
         </VerticalTimelineElement>
