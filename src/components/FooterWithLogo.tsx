0d55f979411fa8ea04b4ff5d0d627cd3c3bc9275
kyoshinoda <kyoshisew@gmail.com>
2023-08-26 22:34:13 -0400

    changed github logo on footer

--- src/components/FooterWithLogo.tsx
+++ src/components/FooterWithLogo.tsx
@@ -1,24 +1,22 @@
 'use client';
 
 import { Footer } from 'flowbite-react';
-
+import { AiFillGithub } from 'react-icons/ai';
 export default function FooterWithLogo() {
   return (
     <Footer container>
       <div className="w-full text-center">
         <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
-          <Footer.Brand
-            alt="Logo"
-            href="https://flowbite.com"
-            name="Flowbite"
-            src="https://flowbite.com/docs/images/logo.svg"
-          />
+          <div className='flex gap-2 items-center'>
+            <AiFillGithub size = {48}/>
+            <h1 className='text-xl font-bold'>Repo</h1>
+          </div>
           <Footer.LinkGroup className='flex gap-3'>
             <Footer.Link href="#">
               About
             </Footer.Link>
             <Footer.Link href="#">
-              Privacy Policy
+              
             </Footer.Link>
             <Footer.Link href="#">
               Licensing
@@ -30,9 +28,9 @@ export default function FooterWithLogo() {
         </div>
         <Footer.Divider />
         <Footer.Copyright
-          by="Flowbite™"
+          by="Kyoshi Noda"
           href="#"
-          year={2022}
+          year={2023}
         />
       </div>
     </Footer>
